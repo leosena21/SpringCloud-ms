fc0b239c903f2e21b810359853c73d73c6f0ba13
Leonardo Sena <leeosena21@gmail.com>
2022-08-07 16:00:58 -0300

    Porta dinamica para o hr-worker + log de porta instância

--- hr-worker/src/main/java/com/projeto/hrworker/services/WorkerService.java
+++ hr-worker/src/main/java/com/projeto/hrworker/services/WorkerService.java
@@ -3,20 +3,27 @@ package com.projeto.hrworker.services;
 import com.projeto.hrworker.dto.WorkerNewInput;
 import com.projeto.hrworker.entities.Worker;
 import com.projeto.hrworker.repositories.WorkerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
 
+@Slf4j
 @Service
 public class WorkerService {
+
+    @Autowired
+    private Environment env;
 
     @Autowired
     WorkerRepository repository;
 
     public Worker findById(Long id) {
+        log.error("PORT = " + env.getProperty("local.server.port"));
         return repository.findById(id)
                 .orElseThrow( () -> new EntityNotFoundException("Deu ruim"));
     }
