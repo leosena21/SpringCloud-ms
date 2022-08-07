5ceb4009996f1121b29e7373ae21c77b713ef598
Leonardo Sena <leeosena21@gmail.com>
2022-08-07 18:31:28 -0300

    Api gateway

--- hr-api-gateway-zuul/src/main/java/com/projeto/hrapigatewayzuul/HrApiGatewayZuulApplication.java
+++ hr-api-gateway-zuul/src/main/java/com/projeto/hrapigatewayzuul/HrApiGatewayZuulApplication.java
@@ -2,7 +2,9 @@ package com.projeto.hrapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrApiGatewayZuulApplication {
 
