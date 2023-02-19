8d5c26f875a79b652a76394ae5e44ad84555f063
Leonardo Sena <leeosena21@gmail.com>
2023-02-19 20:48:51 -0300

    Inserindo logs

--- hr-user/src/main/java/com/projeto/hruser/services/UserService.java
+++ hr-user/src/main/java/com/projeto/hruser/services/UserService.java
@@ -5,14 +5,17 @@ import com.projeto.hruser.entities.User;
 import com.projeto.hruser.repositories.RoleRepository;
 import com.projeto.hruser.repositories.UserRepository;
 import com.projeto.hruser.services.exceptions.ObjectNotFoundException;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static java.lang.String.format;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserService{
@@ -26,16 +29,19 @@ public class UserService{
     private String roleOperatorName;
 
     public User findById(Long id) {
+        log.info(format("Consultando id:%s", id));
         return repository.findById(id)
-                .orElseThrow( () -> new ObjectNotFoundException("User id:" + id + " não encontrado"));
+                .orElseThrow( () -> new ObjectNotFoundException(format("User id:%s não encontrado", id)));
     }
 
     public User findByEmail(String email) {
-        return repository.findByEmail(email).orElseThrow(() -> new ObjectNotFoundException(String.format("Email %s não encontrado", email)));
+        log.info(format("Consultando email:%s", email));
+        return repository.findByEmail(email).orElseThrow(() -> new ObjectNotFoundException(format("Email %s não encontrado", email)));
     }
 
     @Transactional
     public User insert(UserNewInputDTO userDTO) {
+        log.info(format("Preparando para inserir o usuário de email:%s", userDTO.getEmail()));
 
         User user = User.builder()
                 .name(userDTO.getName())
@@ -44,7 +50,7 @@ public class UserService{
                 .build();
 
         user.addRole(roleRepository.findByRoleName(roleOperatorName)
-                .orElseThrow(() -> new ObjectNotFoundException(String.format("Role %s não encontrada", roleOperatorName))));
+                .orElseThrow(() -> new ObjectNotFoundException(format("Role %s não encontrada", roleOperatorName))));
 
         return repository.save(user);
     }
--- hr-worker/src/main/java/com/projeto/hrworker/services/WorkerService.java
+++ hr-worker/src/main/java/com/projeto/hrworker/services/WorkerService.java
@@ -6,15 +6,14 @@ import com.projeto.hrworker.repositories.WorkerRepository;
 import com.projeto.hrworker.services.exceptions.ObjectNotFoundException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 import java.util.List;
 
+import static java.lang.String.format;
 @Slf4j
 @AllArgsConstructor
 @Service
@@ -26,17 +25,21 @@ public class WorkerService {
     WorkerRepository repository;
 
     public Worker findById(Long id) {
+        log.info(format("Consultando worker id:%s", id));
+
         log.error("PORT = " + env.getProperty("local.server.port"));
         return repository.findById(id)
-                .orElseThrow( () -> new ObjectNotFoundException("Worker id:" + id + " não encontrado"));
+                .orElseThrow( () -> new ObjectNotFoundException(format("Worker id:%s não encontrado", id)));
     }
 
     public List<Worker> findAll() {
+        log.info(format("Preparando para buscar todos os workers"));
         return repository.findAll();
     }
 
     @Transactional
     public Worker insert(WorkerNewInput workerDto) {
+        log.info(format("Preparando para inserir o worker de nome: %s", workerDto.getName()));
         return repository.save(
                 Worker.builder()
                         .name(workerDto.getName())
